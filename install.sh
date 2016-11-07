01a8c77649540dea9922bc67d8c85f98ab39d387
bernhard <bernhard.denner@gmail.com>
2016-11-07 19:42:05 +0100

    add install routine

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# get absolute path of our .environment dir
+ENV_DIR=$(readlink -f $(dirname $0))
+
+TARGET_DIR=$HOME
+
+source ${ENV_DIR}/lib/functions.sh
+
+
+# ensure we've got the correct location
+[ -d "${ENV_DIR}" ] || bailout "don't know where I am"
+
+cd $ENV_DIR
+
+
+# install vim environment
+install_env_link vimrc .vimrc
+install_env_link vim .vim
+
+
